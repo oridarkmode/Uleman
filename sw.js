4670c9db009526e1880b9cac62adbc3e8a49ae78
momenkita <gameidakun56@gmail.com>
2026-02-21 11:36:12 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "glass-invite-final-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/script.js",
+  "./data/config.json",
+  "./manifest.webmanifest",
+  // Ikon dasar untuk PWA
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+  // Aset lain akan dicache secara runtime saat diakses
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE ? caches.delete(k) : null)))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then((cached)=>{
+      if(cached) return cached;
+      return fetch(e.request).then((res)=>{
+        const copy = res.clone();
+        // Runtime cache untuk permintaan GET dan origin yang sama
+        try{
+          if(e.request.method==="GET" && new URL(e.request.url).origin===self.location.origin){
+            caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+          }
+        }catch(_){}
+        return res;
+      }).catch(()=>cached);
+    })
+  );
+});
